fcd639cb2e1686121a2d98efb22f04716ebb7a36
Your Name <Your Email/>
2022-09-01 20:45:57 +0800

    豆的提交

--- codes/community/src/main/java/csu/train/community/views/LoginPage.java
+++ codes/community/src/main/java/csu/train/community/views/LoginPage.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 //这是主页，轻易不要动，想动在群里说一下
 public class LoginPage extends Application {
